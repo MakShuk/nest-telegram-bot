4181021389335aa304ba086a12d6acac0528a746
Maksim Shuklin <makshuk@my.com>
2023-10-16 11:55:09 +0300

    +text filter

--- src/bot/bot-update.ts
+++ src/bot/bot-update.ts
@@ -1,8 +1,9 @@
-import { Update, Ctx, Start, Help, Hears, On, Cashtag } from 'nestjs-telegraf';
-import { Context as TelegrafContext } from 'telegraf';
+import { Update, Ctx, Start, Help, Hears, On, Cashtag, Command, InjectBot } from 'nestjs-telegraf';
+import { Telegraf, Context as TelegrafContext } from 'telegraf';
 
 @Update()
 export class BotUpdate {
+	constructor(@InjectBot() private bot: Telegraf<TelegrafContext>) {}
 	@Start()
 	async start(@Ctx() ctx: TelegrafContext) {
 		await ctx.reply('Welcome');
@@ -13,14 +14,23 @@ export class BotUpdate {
 		await ctx.reply('Send me a sticker');
 	}
 
-	@On('sticker')
+	@Command('new')
+	async new(@Ctx() ctx: TelegrafContext) {
+		await ctx.reply('👌');
+	}
+
+	@On('text')
 	async on(@Ctx() ctx: TelegrafContext) {
-		await ctx.reply('👍');
+		console.log(ctx.message);
+		if (ctx.message && 'text' in ctx.message) {
+			ctx.message.text.includes('spam') ? await ctx.reply('No Spam') : await ctx.reply('OK');
+		}
 	}
 
 	@Hears('hi')
 	async hears(@Ctx() ctx: TelegrafContext) {
 		await ctx.reply('Hey there');
+		console.log(this.bot);
 	}
 
 	@Cashtag('$AAPL')
