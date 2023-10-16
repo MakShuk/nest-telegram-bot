6f5e6ed80ccbf91c9a4bea102f83733e87b7d074
Maksim Shuklun <makshuk@bk.ru>
2023-10-16 11:03:45 +0300

    + cashtag method

--- src/bot/bot-update.ts
+++ src/bot/bot-update.ts
@@ -1,4 +1,4 @@
-import { Update, Ctx, Start, Help, On, Hears } from 'nestjs-telegraf';
+import { Update, Ctx, Start, Help, Hears, On, Cashtag } from 'nestjs-telegraf';
 import { Context as TelegrafContext } from 'telegraf';
 
 @Update()
@@ -21,5 +21,10 @@ export class BotUpdate {
 	@Hears('hi')
 	async hears(@Ctx() ctx: TelegrafContext) {
 		await ctx.reply('Hey there');
+	}
+
+	@Cashtag('$AAPL')
+	async apple(@Ctx() ctx: TelegrafContext) {
+		await ctx.reply('Вы упомянули акцию Apple!');
 	}
 }
